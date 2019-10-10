e36fa6d7ad13157821e3dc73c34625d1e5b237f1
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-10 15:53:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/new' do
-<<<<<<< HEAD
     erb :new
-=======
-      erb :new
->>>>>>> b1fa9e6e4670ee5977111a4e1ac9a5e3dee7ba4f
   end
 
   get '/recipes/:id' do
@@ -43,11 +39,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
 
-<<<<<<< HEAD
-  delete '/recipes/:id' do
-=======
   delete '/recipes/:id/delete' do
->>>>>>> b1fa9e6e4670ee5977111a4e1ac9a5e3dee7ba4f
     recipe = Recipe.find(params[:id])
     recipe.delete
     redirect to '/recipes'
